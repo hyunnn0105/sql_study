74bd7c9b8bc00f880f2f663633e190fe83a2b8b4
hyunnn0105 <asha010519@gmail.com>
2022-06-30 18:21:12 +0900

    220639

---
+++ trade.sql
@@ -0,0 +1,33 @@
+
+
+
+CREATE TABLE trade(
+    tra_nu NUMBER(10)
+    , tra_nm VARCHAR2(20) NOT NULL
+    , item VARCHAR2(20) NOT NULL
+    , price NUMBER(10) NOT NULL
+    , tra_phone VARCHAR2(11)
+    , post_method VARCHAR(20)
+    , post_addr VARCHAR2(100)
+    , post_num NUMBER(20)
+);
+
+alter table trade
+add CONSTRAINT trade PRIMARY KEY (tra_nu)
+;
+
+SELECT
+    * FROM trade;
+
+commit;
+    
+    
+INSERT INTO trade VALUES (1, '가나다', '의류', 20000, '01012345678','준등기', '경기도 00시 00구 00아파트 00', 123456789);
+INSERT INTO trade VALUES (2, '나나나', '의류', 25000, '01043215678','반값택배', '서울특별시 00구 00아파트 00', 987654321);
+INSERT INTO trade VALUES (3, '다다다', '음식', 10000, '01043215678','택배', '강원도 00시 00아파트 00','');
+
+commit;
+
+ROLLBACK;
+
+DROP TABLE trade;
---
+++ 학생관리프로그램.sql
@@ -0,0 +1,26 @@
+CREATE TABLE score (
+    stu_num NUMBER(10),
+    stu_name VARCHAR2(20) NOT NULL,
+    kor NUMBER(3) NOT NULL,
+    eng NUMBER(3) NOT NULL,
+    math NUMBER(3) NOT NULL,
+    total NUMBER(3),
+    average NUMBER(5,2),
+    CONSTRAINT pk_score PRIMARY KEY (stu_num)
+);
+
+-- 연속된 숫자 생성 : SEQUENCE table
+
+CREATE SEQUENCE seq_score;
+
+SELECT * FROM score;
+
+-- seq_score.nextval 자동적으로 1번, 2번, 3번 가능
+INSERT INTO score VALUES (seq_score.nextval,  '홍길동', 90, 90, 90, 270, 90);
+INSERT INTO score VALUES (seq_score.nextval,  '김철수', 80, 80, 80, 240, 80);
+INSERT INTO score VALUES (seq_score.nextval,  '박영희', 100, 100, 100, 300, 100);
+COMMIT;
+
+SELECT AVG(average) AS avg_cls
+FROM score
+;
