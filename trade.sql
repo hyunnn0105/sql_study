4bea14132df3764919b6b3eb63dfac9ba6c61759
hyunnn0105 <asha010519@gmail.com>
2022-07-01 17:49:35 +0900

    220701

--- trade.sql
+++ trade.sql
@@ -31,3 +31,25 @@ commit;
 ROLLBACK;
 
 DROP TABLE trade;
+
+
+
+SELECT
+    * FROM trade;
+    
+Alter table trade MODIFY (post_num NUMBER(38));
+
+commit;
+
+TRUNCATE table trade;
+
+Alter table trade MODIFY (post_num VARCHAR2(38));
+
+commit;
+
+INSERT INTO trade VALUES (1, '가나다', '의류', 20000, '01012345678','준등기', '경기도 00시 00구 00아파트 00', '123456789123');
+INSERT INTO trade VALUES (2, '나나나', '의류', 25000, '01043215678','반값택배', '서울특별시 00구 00아파트 00', '987654321123');
+INSERT INTO trade VALUES (3, '다다다', '음식', 10000, '01043215678','택배', '강원도 00시 00아파트 00','');
+
+SELECT
+    * FROM trade;
